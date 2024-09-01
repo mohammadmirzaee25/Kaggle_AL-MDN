789b3946de9b60efd7102842fc3033d6768c9857
mohammadmirzaee25 <94175691+mohammadmirzaee25@users.noreply.github.com>
2024-09-01 17:46:52 +0330

    Update config.py
    change address for kaggl;e

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@
 import os.path
 
 # gets home dir cross platform
-HOME = '/content/al-mdn'
+HOME = '/kaggle/working/al-mdn'
 
 # for making bounding boxes pretty
 COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
